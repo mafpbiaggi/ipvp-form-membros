58409d86880f459883f98eecf0989ac6d030ae7b
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2026-02-04 15:42:34 -0300

    ipvp-form-membros(add): Cria função de armazenamento de dados no banco.

--- app.php
+++ app.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'database.php';
 
 //Função para validação de campos
 function validaCampos($dados, $regras) {
@@ -14,34 +15,147 @@ function validaCampos($dados, $regras) {
     return $erros;
 }
 
+//Função para validação de datas
+function validaData($valor) {
+    return (!empty($valor) && $valor !== '0000-00-00') ? $valor : null;
+}
+
+
+//Função para cadastramento de dados do membro
+function addMembro($dados) {
+    global $host, $user, $pass, $dbname, $port;
+    try {
+        $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
+        $church_id = 22;
+        $user_id = 66;
+
+        $queryProf = "INSERT INTO profissaos (nome, church_id, user_id, created, modified) VALUES (:nome, :church_id, :user_id, NOW(), NOW())";
+
+        $stmtProf = $conn->prepare($queryProf);
+        $stmtProf->bindValue(':nome', trim(mb_strtoupper($dados['profissao'])));
+        $stmtProf->bindValue(':church_id', $church_id);
+        $stmtProf->bindValue(':user_id', $user_id);
+        $stmtProf->execute();
+
+        $profissao_id = $conn->lastInsertId();
+
+        $queryMemb = "INSERT INTO membros (nome, sexo, datanascimento, naturalidade, email, cel, fone, rg, cpf, estadocivil,
+                    datacasamento, nomeconjuge, escolaridade_id, profissao_id, batizado, databatismo, pastorbatismo, igrejabatismo, profissaofe,
+                    dataprofe, pastorprofe, igrejaprofe, nomemae, nomepai, church_id, user_id, tipo, created, modified) VALUES (:nome, :sexo,
+                    :datanascimento, :naturalidade, :email, :cel, :fone, :rg, :cpf, :estadocivil, :datacasamento, :nomeconjuge, :escolaridade_id,
+                    :profissao_id, :batizado, :databatismo, :pastorbatismo, :igrejabatismo, :profissaofe, :dataprofe, :pastorprofe, :igrejaprofe,
+                    :nomemae, :nomepai, :church_id, :user_id, 'Membro', NOW(), NOW())";
+
+        $datacasamento = validaData($dados['datacasamento'] ?? null);
+        $databatismo  = validaData($dados['databatismo'] ?? null);
+        $dataprofe    = validaData($dados['dataprofe'] ?? null);
+
+        $stmtMemb = $conn->prepare($queryMemb);
+        $stmtMemb->bindValue(':nome', trim(mb_strtoupper($dados['nome'])));
+        $stmtMemb->bindValue(':sexo', $dados['sexo']);
+        $stmtMemb->bindValue(':datanascimento', $dados['datanascimento']);
+        $stmtMemb->bindValue(':naturalidade', trim(mb_strtolower($dados['naturalidade'])));
+        $stmtMemb->bindValue(':email', trim(mb_strtolower($dados['email'])));
+        $stmtMemb->bindValue(':cel', $dados['cel']);
+        $stmtMemb->bindValue(':fone', $dados['fone']);
+        $stmtMemb->bindValue(':rg', $dados['rg']);
+        $stmtMemb->bindValue(':cpf', $dados['cpf']);
+        $stmtMemb->bindValue(':estadocivil', $dados['estadocivil']);
+        $stmtMemb->bindValue(':datacasamento', $datacasamento);
+        $stmtMemb->bindValue(':nomeconjuge', trim(mb_strtoupper($dados['nomeconjuge'])));
+        $stmtMemb->bindValue(':escolaridade_id', $dados['escolaridade']);
+        $stmtMemb->bindValue(':profissao_id', $profissao_id);
+        $stmtMemb->bindValue(':batizado', $dados['batizado']);
+        $stmtMemb->bindValue(':databatismo', $databatismo);
+        $stmtMemb->bindValue(':pastorbatismo', trim(mb_strtoupper($dados['pastorbatismo'])));
+        $stmtMemb->bindValue(':igrejabatismo', trim(mb_strtoupper($dados['igrejabatismo'])));
+        $stmtMemb->bindValue(':profissaofe', $dados['profissaofe']);
+        $stmtMemb->bindValue(':dataprofe', $dataprofe);
+        $stmtMemb->bindValue(':pastorprofe', trim(mb_strtoupper($dados['pastorprofe'])));
+        $stmtMemb->bindValue(':igrejaprofe', trim(mb_strtoupper($dados['igrejaprofe'])));
+        $stmtMemb->bindValue(':nomemae', trim(mb_strtoupper($dados['nomemae'])));
+        $stmtMemb->bindValue(':nomepai', trim(mb_strtoupper($dados['nomepai'])));
+        $stmtMemb->bindValue(':church_id', $church_id);
+        $stmtMemb->bindValue(':user_id', $user_id);
+        $stmtMemb->execute();
+
+        $membro_id = $conn->lastInsertId();
+
+        $queryEnd = "INSERT INTO enderecos (logradouro, numero, complemento, bairro, cep, cidade, estado, membro_id, user_id, church_id, created, modified) 
+        VALUES (:logradouro, :numero, :complemento, :bairro, :cep, :cidade, :estado, :membro_id, :user_id, :church_id, NOW(), NOW())";
+
+        $stmtEnd = $conn->prepare($queryEnd);
+        $stmtEnd->bindValue(':logradouro', trim(mb_strtoupper($dados['logradouro'])));
+        $stmtEnd->bindValue(':numero', $dados['numero']);
+        $stmtEnd->bindValue(':complemento', trim(mb_strtoupper($dados['complemento'])));
+        $stmtEnd->bindValue(':cep', $dados['cep']);
+        $stmtEnd->bindValue(':bairro', trim(mb_strtoupper($dados['bairro'])));
+        $stmtEnd->bindValue(':cidade', trim(mb_strtoupper($dados['cidade'])));
+        $stmtEnd->bindValue(':estado', $dados['estado']);
+        $stmtEnd->bindValue(':membro_id', $membro_id);
+        $stmtEnd->bindValue(':user_id', $user_id);
+        $stmtEnd->bindValue(':church_id', $church_id);
+        $stmtEnd->execute();
+
+        return ['status' => true, 'msg' => "Dados enviados com sucesso."];
+    
+    } catch (PDOException $err) {
+        return ['status' => false, 'msg' => "Não foi possível finalizar o cadastramento.<br>Contate o administrador."];
+    }
+}
+
 $regras = [
     'nome' => ['required' => true, 'max' => 100],
     'sexo' => ['required' => true],
     'datanascimento' => ['required' => true],
     'naturalidade' => ['required' => true],
-    'rg' => ['required' => true, 'max' => 12],
-    'cpf' => ['required' => true, 'max' => 14],
-    'estadocivil' => ['required' => true],
-    'escolaridade' => ['required' => true],
-    'profissao' => ['required' => true, 'max' => 50],
-    'batizado' => ['required' => true],
-    'profissaofe' => ['required' => true],
+    
     'logradouro' => ['required' => true, 'max' => 50],
     'numero' => ['required' => true],
     'bairro' => ['required' => true, 'max' => 30],
     'cep' => ['required' => true, 'max' => 9],
     'cidade' => ['required' => true],
     'estado' => ['required' => true, 'max' => 2],
+    
+    'email' => ['required' => false],
+    'cel' => ['required' => false],
+    'fone' => ['required' => false],
+
+    'rg' => ['required' => true, 'max' => 12],
+    'cpf' => ['required' => true, 'max' => 14],
+    'estadocivil' => ['required' => true],
+    'datacasamento' => ['required' => false],
+    'nomeconjuge' => ['required' => false],
+    'nomemae' => ['required' => false],
+    'nomepai' => ['required' => false],
+    'escolaridade' => ['required' => true],
+    'profissao' => ['required' => true, 'max' => 80],
+    
+    'batizado' => ['required' => true],
+    'databatismo' => ['required' => false],
+    'pastorbatismo' => ['required' => false],
+    'igrejabatismo' => ['required' => false],
+    
+    'profissaofe' => ['required' => true],
+    'dataprofe' => ['required' => false],
+    'pastorprofe' => ['required' => false],
+    'igrejaprofe' => ['required' => false], 
 ];
 
 //Recebe os dados do formulário.
 $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 $erros = validaCampos($dados, $regras);
 
+foreach ($regras as $campo => $regra) {
+    if (!isset($dados[$campo])) {
+        $dados[$campo] = '';
+    }
+}
+
 if ($erros) {
    echo json_encode(['status' => false, 'msg' => implode('<br>', $erros)]);
    exit;
 }
 
-$retorna = ['status' => true, 'msg' => "Dados enviados com sucesso."];
+$retorna = addMembro($dados);
 echo json_encode($retorna);
